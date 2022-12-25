a0f44a1746136aa93383596d64f5315c1a6883cf
Marcos <3091977221@qq.com>
2022-12-26 00:06:28 +0800

    新增获取 QQ 头像，QQ 个性签名，背景音乐相关功能

---
+++ src/main/java/com/marcos/blog/controller/IndexController.java
@@ -0,0 +1,43 @@
+package com.marcos.blog.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author: marcos
+ * @createDate: 2022/12/25
+*/
+@RestController
+@RequestMapping("/api")
+public class IndexController {
+
+	@Value("${user.config.qq}")
+	private String qq;
+
+	@Value("${user.config.signature}")
+	private String signature;
+
+	@Value("${user.config.background_music}")
+	private String music;
+	
+	/** 获取 QQ 头像链接 */
+	@GetMapping("/getIcon")
+	public String getIcon(){
+		return String.format("https://q1.qlogo.cn/g?b=qq&nk=%s&s=640", qq);
+	}
+
+	/** 获取 QQ 个性签名 */
+	@GetMapping("/getSignature")
+	public String getSignature(){
+		return signature;
+	}
+
+	/** 获取背景音乐 */
+	@GetMapping("getMusic")
+	public String getMusic(){
+		return music;
+	}
+	
+}
